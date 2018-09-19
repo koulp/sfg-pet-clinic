6d59fb878fc0652d5005b464bf3deaa6c28ec6a7
koulp <vkoulp@gmail.com>
2018-09-19 17:10:25 +0300

    Added PetTypeMapService

---
+++ pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package com.springframework.sfgpetclinic.services;
+
+import com.springframework.sfgpetclinic.model.PetType;
+import com.springframework.sfgpetclinic.services.CrudService;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
